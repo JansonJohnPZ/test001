b245ea5a540e5240481bd0aa23ad2467e6f13178
Ryan <jansonjohn@protonmail.com>
2020-02-29 12:26:15 +0800

    aaaa22

--- a001.py
+++ a001.py
@@ -1,8 +1,22 @@
-import numpy as np
-import cv2 as cv
-import matplotlib.pyplot as plt
+def findAll(str, cha):
+    tip = 0
+    en = []
+    while(True):
+        temp = str.find(cha, tip)
+        if temp == -1:
+            tip += 1
+            temp = str.find(cha, tip)
+            if temp == -1:
+                break
+        else:
+            en.append(temp)
+            tip = temp + 1
+    return en
 
-a = np.array([1,2,3])
-b = 3
-print(a)
-print(b)
+str = 'abcd' * 12
+print(str)
+en = findAll(str, 'a')
+print(en)
+
+for i in en:
+    print(str[i])
